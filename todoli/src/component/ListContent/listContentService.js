1777e430e63c4bfae2b221652305adcbfa63dca3
Teezey <yibiaojob@gmail.com>
2023-02-03 20:15:10 -0800

    empty input after add task

--- todoli/src/component/ListContent/listContentService.js
+++ todoli/src/component/ListContent/listContentService.js
@@ -20,6 +20,7 @@ function ContentService (props){
         }
         else{
             setStuff([...stuffInCategoryArray,userInput]);
+            document.getElementById("myinput").value="";
         }
     }
 
@@ -60,7 +61,7 @@ function ContentService (props){
                 <div className="card shadow p-3 mb-5 bg-body rounded mx-auto">
                 <h5 className="card-header">
                     <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Enter Task here"
+                    <input id="myinput" type="text" className="form-control" placeholder="Enter Task here"
                      aria-label="Task" aria-describedby="basic-addon1" maxLength="60"
                      onChange={handleInputChange}></input>
                     <span><button type="button" className="btn btn-success ms-2" onClick={handleAddTask}>Add Task</button></span>
